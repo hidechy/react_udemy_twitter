562e33ac3a9471f6b4bb3c99a8ae6a47cc0eef0e
toyohide <hide.toyoda@gmail.com>
2023-09-25 15:09:50 +0900

    20230925 feed create start

---
+++ src/components/TweetInput.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import styles from './TweetInput.module.css'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
+import { selectUser } from '../features/userSlice'
+import { auth } from '../firebase'
+import { Avatar } from '@material-ui/core'
+
+const TweetInput = () => {
+
+    const user = useSelector(selectUser);
+
+  return (
+    <div>
+        <Avatar 
+            className={styles.tweet_avatar}
+            src={user.photoUrl}
+            onClick={async () => {
+                await auth.signOut();
+            }}
+        />
+    </div>
+  )
+}
+
+export default TweetInput
